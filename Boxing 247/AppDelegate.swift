8d87aafa9d3f82e3b1fb7c36bd0a2770422c7ba4
Omar Amoako <>
2021-08-26 01:32:28 +0100

    Updated AppDelegate

--- Boxing 247/AppDelegate.swift
+++ Boxing 247/AppDelegate.swift
@@ -15,20 +15,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        Instabug.start(withToken: "4c964612828aa87d11f54c754458ca63", invocationEvents: [.shake, .screenshot])
-
-        
+    
+        configureCrashReporter()
+        configureGlobalUIAppearanceSettings()
         //deleteOldNewsArticles()
+        return true
+    }
+    
+    func configureCrashReporter() {
+        Instabug.start(withToken: "4c964612828aa87d11f54c754458ca63", invocationEvents: [.shake, .screenshot])
+    }
+    
+    func configureGlobalUIAppearanceSettings() {
         
         let attributes = [NSAttributedString.Key.font:  UIFont(name: "AppleSDGothicNeo-UltraLight", size: 38.0)!, NSAttributedString.Key.foregroundColor: white247]
         UINavigationBar.appearance().backgroundColor = base247
         UINavigationBar.appearance().prefersLargeTitles = true
         UINavigationBar.appearance().largeTitleTextAttributes = attributes
         UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key(rawValue: NSAttributedString.Key.foregroundColor.rawValue): white247]
+        UIScrollView.appearance().indicatorStyle = .black
         window?.backgroundColor = base247
-       
-        return true
     }
 
     func deleteOldNewsArticles() {
