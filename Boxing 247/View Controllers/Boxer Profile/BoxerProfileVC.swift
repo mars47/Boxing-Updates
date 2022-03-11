1232d8d1884a157fbe60103c6ceb48cb72e243a9
Omar Amoako <>
2022-03-11 16:06:36 +0000

    Added country assets

---
+++ Boxing 247/View Controllers/Boxer Profile/BoxerProfileVC.swift
@@ -0,0 +1,30 @@
+//
+//  BoxerProfileVC.swift
+//  Boxing 247
+//
+//  Created by Omar on 11/03/2022.
+//  Copyright © 2022 Omar. All rights reserved.
+//
+
+import UIKit
+
+class BoxerProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
